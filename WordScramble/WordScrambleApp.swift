2fadaec660c52997ec3ff2aff06ee625b9ee4dd3
Leo Chung <leo.chung97@gmail.com>
2023-12-07 12:42:48 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Leo Chung on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
